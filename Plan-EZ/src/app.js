9db034fafb36e6e3014224cf39d9da6ad50f93f3
Liam Ormiston <lormiston@btbcku.com>
2017-09-14 17:35:14 -0500

    Added UI prompt to receive event name from user

--- Plan-EZ/src/app.js
+++ Plan-EZ/src/app.js
@@ -1,21 +1,24 @@
 import DefineMap from 'can-define/map/';
 import route from 'can-route';
 import 'can-route-pushstate';
+import 'can-stache-bindings';
 
 const AppViewModel = DefineMap.extend({
   message: {
     value: 'Welcome to our easy event planner!',
     serialize: false
   },
-  Name () {
-    eventName = promt("Please enter the name of your event", "Event name...");
+  eventName: {
+  },
+  name() {
+    eventName = prompt("Please enter the name of your event", "Event name");
     return (eventName);
   },
   title: {
     value: 'Plan-EZ',
     serialize: false
-  }
-  
+  },
+
 class event(){
   constructor(name, attendees, time, availability){
     this.name = name;
