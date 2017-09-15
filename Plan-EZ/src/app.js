d0773461f901466c04a162319bb55a87220bb26b
Patrick Canny <patrickcanny@ku.edu>
2017-09-14 23:27:40 -0500

    Editing event class structure and fixing a small syntax bug

--- Plan-EZ/src/app.js
+++ Plan-EZ/src/app.js
@@ -4,55 +4,70 @@ import 'can-route-pushstate';
 import 'can-stache-bindings';
 
 const AppViewModel = DefineMap.extend({
-  message: {
-    value: 'Welcome to our easy event planner!',
-    serialize: false
-  },
-  eventName: {
-  },
-  name() {
-    eventName = prompt("Please enter the name of your event", "Event name");
-    return (eventName);
-  },
-  title: {
-    value: 'Plan-EZ',
-    serialize: false
-  },
+      message: {
+        value: 'Welcome to our easy event planner!',
+        serialize: false
+      },
 
-class event(){
-  constructor(name, attendees, time, availability){
-    this.name = name;
-    this.attendees = attendees;
-    this.time = time;
-    this.availability = availability;
-  }
-  getName(){
-    return this.name;
-  }
-  getAttendees(){
-    return this.attendees;
-  }
-  getTime(){
-    return this.time;
-  }
-  getAvailability(){
-    return this.availability;
-  }
-  setName(){
-    name = Name();
-  }
-  setAttendees(){
-    attendees = Attendees();
-  }
-  setTime(){
-    time = Time();
-  }
-  setAvailability(){
-    availability = Availability();
-  }
+      //TODO: This is currently broken with error:
+      //"Unable to find Key or Helper "eventName""
+      eventName: {},
+      name() {
+        eventName = prompt("Please enter the name of your event", "Event name");
+        return (eventName);
+      },
+      title: {
+        value: 'Plan-EZ',
+        serialize: false
+      },
+    });
 
-}
+      //Event Class Definition
+      //TODO: UI for:
+       //                     Name(), Time(), Availability()
+       //
+      var event = (function() {
+        //"private variables" for new event objects
+        var eventName, eventAttendees, eventAvailability, eventTime;
 
-});
+        // class "constructor"
+        function event(newEventName) {
+          eventName = newEventName;
+        };
 
-export default AppViewModel;
+        //adding methods to prototype so all event instances access
+        //private methods
+        event.prototype.getName = function() {
+          return this.eventName;
+        };
+        event.prototype.getAttendees = function() {
+          return this.eventAttendees;
+        };
+
+        event.prototype.getTime = function() {
+          return this.eventTime;
+        };
+
+        event.prototype.getAvailability = function() {
+          return this.eventAvailability;
+        };
+
+        event.prototype.setName = function() {
+          this.eventName = name();
+        };
+
+        event.prototype.setAttendees = function() {
+          this.eventAttendees = Attendees();
+        };
+
+        event.prototype.setTime = function() {
+          this.eventTime = Time();
+        };
+
+        event.prototype.setAvailability = function() {
+          this.eventAvailability = Availability();
+        };
+        return event;
+      })();
+
+      export default AppViewModel;
