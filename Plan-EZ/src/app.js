fb351d45b34a4fa712f177a7e67ca55a527ea76d
LiamOrmiston <ljormiston@ku.edu>
2017-09-16 17:31:01 -0500

    start from scratch. input fields, global variable initializer, test method

--- Plan-EZ/src/app.js
+++ Plan-EZ/src/app.js
@@ -3,67 +3,33 @@ import route from 'can-route';
 import 'can-route-pushstate';
 import 'can-stache-bindings';
 
+var newName = "";
 const AppViewModel = DefineMap.extend({
       message: {
         value: 'Welcome to our easy event planner!',
         serialize: false
       },
-
-      //TODO: This is currently broken with error:
-      //"Unable to find Key or Helper "eventName""
-      eventName: {},
-      name() {
-        eventName = prompt("Please enter the name of your event", "Event name");
-        return (eventName);
+      // initializing variables as global variables
+      newEvent() {
+        window.name = document.getElementById("eventName").value;
+        window.month = document.getElementById("eventMonth").value;
+        window.day = document.getElementById("eventDay").value;
+        window.timeStart = document.getElementById("eventStart").value;
+        window.timeEnd = document.getElementById("eventEnd").value;
+      },
+      // test function for global variables
+      printItems () {
+        console.log(name);
+        console.log(month);
+        console.log(day);
+        console.log(timeStart);
+        console.log(timeEnd);
       },
       title: {
         value: 'Plan-EZ',
         serialize: false
       },
     });
-
-      var timeSet = (function() {
-
-        // private variables
-        var month, day, time;
-
-        // class "constructor"
-        function time(eventMonth,eventDay,eventTimes) {
-          month = eventMonth;
-          day = eventDay;
-          time = eventTimes;
-        };
-
-        // member methods
-        // TODO: match preference with setting function? not sure how the
-        //       UI handles the interaction
-        time.prototype.getMonth = function() {
-          return this.month;
-        };
-
-        time.prototype.setMonth = function(newMonth) {
-          this.month = newMonth;
-        };
-
-        time.prototype.getDay = function() {
-          return this.day;
-        };
-
-        time.prototype.setDay = function(newDay) {
-          this.day = newDay;
-        };
-
-        time.prototype.getTime = function() {
-          return this.time;
-        };
-
-        time.prototype.setTime = function(newTime) {
-          this.time = newTime;
-        };
-
-        return timeSet;
-
-      })();
 
       //Event Class Definition
       //TODO: UI for:
